c5f801959ead396474b6392a355929463aee2b60
Undead9000 <undead10000@gmail.com>
2021-01-22 15:43:19 +0200

    Add mobile version for shop page

--- resources/views/woocommerce/content-product.blade.php
+++ resources/views/woocommerce/content-product.blade.php
@@ -1,19 +1,11 @@
 <li>
-  @php
-    /*do_action( 'woocommerce_before_shop_loop_item' );
-    do_action( 'woocommerce_before_shop_loop_item_title' );
-    do_action( 'woocommerce_shop_loop_item_title' );
-    do_action( 'woocommerce_after_shop_loop_item_title' );
-    do_action( 'woocommerce_after_shop_loop_item' );*/
-  @endphp
+  <a href="@php the_permalink(); @endphp" class="shop-content__item">
+    @php do_action( 'woocommerce_before_shop_loop_item_title' ) @endphp
 
-    <a href="@php the_permalink(); @endphp" class="shop-content__item">
-        @php do_action( 'woocommerce_before_shop_loop_item_title' ) @endphp
-
-        <div class="shop-content__details">
-            <h3>@php the_title() @endphp</h3>
-            @php do_action( 'woocommerce_after_shop_loop_item_title' ) @endphp
-            @include('woocommerce.loop.discount-message')
-        </div>         
-    </a>     
+    <div class="shop-content__details">
+      <h3>@php the_title() @endphp</h3>
+      @php do_action( 'woocommerce_after_shop_loop_item_title' ) @endphp
+      @include('woocommerce.loop.discount-message')
+    </div>         
+  </a>     
 </li>
